90cfbc75f4dcfdd71a4c6f6de24f9417cb3253f8
Victor Wiedemann <victor.wiedemann@gmail.com>
2020-05-18 08:49:45 -0700

    starting C# advanced class

---
+++ CSharpUdemyAdvanced/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpUdemyAdvanced
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
